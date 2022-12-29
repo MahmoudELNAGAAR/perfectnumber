7d2f513e8fec403e05c4f6a6ad505ff5845ec0a8
MahmoudELNAGAAR <121632879+MahmoudELNAGAAR@users.noreply.github.com>
2022-12-29 19:38:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PerfectNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter n1: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter n2: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("Perfect numbers between {0} and {1}:", n1, n2);
+            for (int i = n1; i <= n2; i++)
+            {
+                if (IsPerfect(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool IsPerfect(int n)
+        {
+            
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            
+            return sum == n;
+        }
+    }
+}
+
